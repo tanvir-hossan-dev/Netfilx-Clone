b62d606fb84e04112da8da320fabe1c43a9557c1
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-10-06 23:28:18 +0600

    folder structure done and create component

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import Video from "../components/Row/Video";
+
+function App() {
+  return (
+    <>
+      <Video title="Netfilx Originals" />
+      <Video title="Top Trending" />
+    </>
+  );
+}
+
+export default App;
