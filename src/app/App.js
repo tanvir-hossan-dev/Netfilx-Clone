e16649a11a0bb522cc72e3eb488f388f937594e1
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-10-06 23:33:54 +0600

    get data from url

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import Video from "../components/Row/Video";
+import { Requests } from "../Requests";
 
 function App() {
   return (
     <>
-      <Video title="Netfilx Originals" />
-      <Video title="Top Trending" />
+      <Video title="Netfilx Originals" fetchUrl={Requests.fetchNetflixOriginals} />
+      <Video title="Top Trending" fetchUrl={Requests.fetchTrending} />
     </>
   );
 }
--- src/components/Row/Video.jsx
+++ src/components/Row/Video.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Instance } from "../../Instance";
 
-const Video = ({ title }) => {
+const Video = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await Instance.get(fetchUrl);
+      setMovies(data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
   return (
     <div>
       <h2>{title}</h2>
