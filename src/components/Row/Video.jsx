6d9f0b1bfaa1dedd7aad05d42880403c1c492bb1
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-10-08 01:45:31 +0600

    movie trailer video component setup done

--- src/components/Row/Video.jsx
+++ src/components/Row/Video.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Instance } from "../../Instance";
 import "./Video.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Video = ({ title, fetchUrl, isSmall }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   const base_url = "https://image.tmdb.org/t/p/original";
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -13,6 +25,21 @@ const Video = ({ title, fetchUrl, isSmall }) => {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const handleMovie = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => {});
+    }
+  };
+
+  console.log(movies);
   return (
     <div>
       <h2 className="text-3xl text-white font-bold pb-4">{title}</h2>
@@ -21,6 +48,7 @@ const Video = ({ title, fetchUrl, isSmall }) => {
           <div key={movie.id} className="mx-[5px]">
             <img
               src={`${base_url}${isSmall ? movie.poster_path : movie.backdrop_path}`}
+              onClick={() => handleMovie(movie)}
               alt=""
               className={`w-full ${isSmall ? "max-h-[250px]" : "max-h-[100px]"}    object-contain  transition
               duration-500 hover:scale-[1.2] cursor-pointer overflow-hidden`}
@@ -28,6 +56,7 @@ const Video = ({ title, fetchUrl, isSmall }) => {
           </div>
         ))}
       </div>
+      {trailerUrl && <YouTube className="py-4" videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
